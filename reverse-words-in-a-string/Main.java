99aa3ca11b3063828f6ddaf36394299673997aaa
Nate Schainblatt <nschainblatt@gmail.com>
2025-03-15 17:27:22 -0400

    alternative solution using two pointers

--- reverse-words-in-a-string/Main.java
+++ reverse-words-in-a-string/Main.java
@@ -8,11 +8,13 @@ public class Main {
 		String expected = "world hello";
 		String actual = s.reverseWords(input);
 		assert expected.equals(actual) : String.format("expected: '%s' actual: '%s'", expected, actual);
+		System.out.println("1. passed");
 
 		input = "the sky is blue";
 		expected = "blue is sky the";
 		actual = s.reverseWords(input);
 		assert expected.equals(actual) : String.format("expected: '%s' actual: '%s'", expected, actual);
+		System.out.println("2. passed");
 
 	}
 }
@@ -20,26 +22,30 @@ public class Main {
 class Solution {
 	public String reverseWords(String s) {
 		StringBuilder builder = new StringBuilder();
-
-		boolean inWord = false;
-		StringBuilder subBuilder = new StringBuilder();
+		int left = -1;
+		int right = -1;
 
 		for (int i = 0; i < s.length(); i++) {
 			char curr = s.charAt(i);
 			if (curr == ' ') {
-				if (inWord) {
-					builder.insert(0, subBuilder.toString() + " ");
-					subBuilder.delete(0, subBuilder.length());
+				if (right != -1) { // encountered a space and right is set, were in a word
+					builder.insert(0, s.substring(left, right+1) + " ");
+					left = -1;
+					right = -1;
 				}
-				inWord = false;
 				continue;
+			} else {
+				// in a word
+				right = i;
+				if (left == -1) { // only set left at the beginning of a word
+					left = i;
+				}
 			}
-
-			inWord = true;
-			subBuilder.append(curr);
 		}
 
-		builder.insert(0, subBuilder.toString() + " ");
+		if (left != -1 && right != -1) { // add any leftover if the last word didn't have a space after it
+			builder.insert(0, s.substring(left, right+1) + " ");
+		}
 
 		return builder.toString().trim();
 	}
