118c54b95d0b104b1cf5202f6745341141ed19ae
Nate Schainblatt <nschainblatt@gmail.com>
2025-03-14 21:13:59 -0400

    improve solution

--- integer-to-roman/Main.java
+++ integer-to-roman/Main.java
@@ -8,11 +8,6 @@ public class Main {
 
 		Solution s = new Solution();
 
-		// Tests
-		assert s.getFirstDigit(1234) == 1;
-		assert s.getFirstDigit(4321) == 4;
-		assert s.getFirstDigit(40) == 4 : s.getFirstDigit(40);
-
 		String actual = s.intToRoman(3749);
 		String expected = "MMMDCCXLIX";
 		assert expected.equals(actual) : String.format("expected: %s actual: %s", expected, actual);
@@ -60,74 +55,25 @@ public class Main {
 }
 
 class Solution {
-
-	public int[] integers = new int[] {1, 5, 10, 50, 100, 500, 1000};
-
-	public int getFirstDigit(int num) {
-		while ((int) num > 9) {
-			num /= 10;
-		}
-		return (int) num;
-	}
-
 	public String intToRoman(int num) {
 		String roman = "";
+		String[] romanNumerals = { "I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M" };
+		int[] integerValues = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000 };
 
-		while (num > 0) {
-			int firstDigit = getFirstDigit(num);
-			int index = findIndexOfMaxRomanToSubtract(num);
+		int index = romanNumerals.length - 1;
+		while (index >= 0 && num > 0) {
+			int integer = integerValues[index];
+			String newRoman = romanNumerals[index];
 
-			int sub = 0;
-			String newRoman = "";
-
-			if (firstDigit == 4) {
-				// Next half power of 10 minus maximum number to subract e.g. if num is 4xx 500 - 100 or num is 4x 50 - 10
-				char prefix = getRomanFromInteger(integers[index]); //10
-				char postfix = getRomanFromInteger(integers[index+1]); //50
-				sub = integers[index+1] - integers[index];
-				newRoman = new String(new char[] {prefix, postfix});
-			} else if (firstDigit == 9) {
-				// Next power of  10 number minus previous power of 10 number e.g. 1000 - 100 or 100 - 10
-				char prefix = getRomanFromInteger(integers[index-1]);
-				char postfix = getRomanFromInteger(integers[index+1]);
-				sub = integers[index+1] - integers[index-1];
-				newRoman = new String(new char[] {prefix, postfix});
-			} else {
-				// Subtract the highest possible number
-				sub = integers[index];
-				newRoman += getRomanFromInteger(sub);
+			int q = num / integer;
+			for (int i = 0; i < q; i++) {
+				roman += newRoman;
 			}
 
-			num -= sub;
-			roman += newRoman;
-
-		}
-
-		return roman;
-	}
-
-
-	private int findIndexOfMaxRomanToSubtract(int num) {
-		int index = integers.length - 1;
-		while (num - integers[index] < 0 && index >= 0) {
+			num %= integer;
 			index--;
 		}
 
-		return index;
-	}
-
-	private char getRomanFromInteger(int r) {
-		return switch (r) {
-			case 1 -> 'I';
-			case 5 -> 'V';
-			case 10 -> 'X';
-			case 50 -> 'L';
-			case 100 -> 'C';
-			case 500 -> 'D';
-			case 1000 -> 'M';
-			default -> {
-				throw new RuntimeException();
-			}
-		};
+		return roman;
 	}
 }
