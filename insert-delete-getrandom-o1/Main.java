484f2c05accde17794d0c3cd2b1919e76341a3ad
nate <nschainblatt@gmail.com>
2025-03-24 21:20:00 +0000

    improve solution

--- insert-delete-getrandom-o1/Main.java
+++ insert-delete-getrandom-o1/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
 		assert obj.contains(-1);
 		assert obj.contains(100);
+		obj.print();
 		assert obj.remove(100);
 		assert obj.remove(-1);
 		assert !obj.contains(100);
@@ -27,112 +28,54 @@ public class Main {
 	}
 }
 
+// Hash Map and a List
+// Hash map key is the value being inserted, the value is the index it is being
+// added to in the list
+// When removing an item, swap the index to remove with the last item in the
+// list, and then finally delete the last element in the list
+// to achieve removal in constant time.
+
 class RandomizedSet {
 
-	private List<List<Integer>> bucketHolder = new ArrayList<>();
-	private final int defaultSize = 16;
-	private int entryCount = 0;
-	private final double maxLoadFactor = 0.75;
+	private final Map<Integer, Integer> map = new HashMap<>();
+	private final List<Integer> list = new ArrayList<>();
 	private final Random r = new Random();
 
-	public void print() {
-		System.out.printf("Buckets: %d, Entries: %d, Load Factor: %f, Data: %s\n", bucketHolder.size(),
-				entryCount, getLoadFactor(), bucketHolder);
-	}
-
 	public RandomizedSet() {
-		expandBucketHolder(defaultSize);
 	}
 
-	private double getLoadFactor() {
-		return entryCount / (double) bucketHolder.size();
+	public void print() {
+		System.out.printf("MAP: %s, LIST: %s", map, list);
 	}
 
 	public boolean insert(int val) {
-		int hashKey = hashValue(val);
-		List<Integer> bucket = bucketHolder.get(hashKey);
-		for (int num : bucket) {
-			if (num == val) {
-				return false;
-			}
+		if (map.containsKey(val)) {
+			return false;
 		}
-		bucket.add(val);
-		entryCount++;
-		// Check if we need to expand and re-hash
-		if (getLoadFactor() > maxLoadFactor) {
-			expandBucketHolder(bucketHolder.size());
-			rehash();
-		}
+		int index = list.size(); // This is the future index it will be in the list
+		list.add(val);
+		map.put(val, index);
 		return true;
 	}
 
-	private void expandBucketHolder(int size) {
-		for (int i = 0; i < size; i++) {
-			bucketHolder.add(new ArrayList<>());
-		}
-	}
-
-	private void rehash() {
-		List<List<Integer>> newBucketHolder = new ArrayList<>();
-		for (int i = 0; i < bucketHolder.size(); i++) {
-			newBucketHolder.add(new ArrayList<>());
-		}
-
-		for (List<Integer> bucket : bucketHolder) {
-			for (int val : bucket) {
-				int hashKey = hashValue(val);
-				List<Integer> newBucket = newBucketHolder.get(hashKey);
-				newBucket.add(val);
-			}
-		}
-
-		bucketHolder = new ArrayList<>(newBucketHolder);
-	}
-
 	public boolean remove(int val) {
-		int hashKey = hashValue(val);
-		List<Integer> bucket = bucketHolder.get(hashKey);
-		for (int i = 0; i < bucket.size(); i++) {
-			if (bucket.get(i) == val) {
-				bucket.remove(i);
-				entryCount--;
-				return true;
-			}
+		if (map.containsKey(val) && !list.isEmpty()) {
+			int index = map.get(val);
+			int lastVal = list.get(list.size() - 1);
+			list.set(index, lastVal);
+			map.put(lastVal, index);
+			list.remove(list.size() - 1);
+			map.remove(val);
+			return true;
 		}
 		return false;
 	}
 
 	public boolean contains(int val) {
-		int hashKey = hashValue(val);
-		List<Integer> bucket = bucketHolder.get(hashKey);
-		for (int num : bucket) {
-			if (num == val) {
-				return true;
-			}
-		}
-		return false;
+		return map.containsKey(val);
 	}
 
 	public int getRandom() {
-		List<Integer> bucket;
-		while (true) {
-			int randomBucketIndex = r.nextInt(bucketHolder.size());
-			List<Integer> randomBucket = bucketHolder.get(randomBucketIndex);
-			if (randomBucket.isEmpty()) {
-				continue;
-			} else {
-				bucket = randomBucket;
-				break;
-			}
-		}
-		int randomIndex = r.nextInt(bucket.size());
-		return bucket.get(randomIndex);
-	}
-
-	private int hashValue(int value) {
-		while (value < 0) {
-			value += bucketHolder.size();
-		}
-		return value % bucketHolder.size();
+		return list.get(r.nextInt(list.size()));
 	}
 }
