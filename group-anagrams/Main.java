a35fc071d18e612b3362600ca588d5aba1280b08
nate <nschainblatt@gmail.com>
2025-04-01 22:36:17 +0000

    improve solution

--- group-anagrams/Main.java
+++ group-anagrams/Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		Solution s = new Solution();
+		s.groupAnagrams(new String[] { "eat", "tea", "c", "a", "", "", "abc", "cab" });
 		// No assertions since the description allows any order to be returned.
 	}
 }
@@ -10,16 +11,21 @@ public class Main {
 class Solution {
 	public List<List<String>> groupAnagrams(String[] strs) {
 		List<List<String>> anagrams = new ArrayList<>();
-		Map<List<Character>, Integer> indicator = new HashMap<>();
+		Map<List<Integer>, Integer> indicator = new HashMap<>();
 
-		for (String s : strs) {
-			List<Character> chars = s.chars().sorted().mapToObj(i -> (char) i).toList();
-			if (indicator.containsKey(chars)) {
-				anagrams.get(indicator.get(chars)).add(s);
+		for (int i = 0; i < strs.length; i++) {
+			List<Integer> characterCounter = new ArrayList<>(Collections.nCopies(26, 0));
+			String s = strs[i];
+			for (int j = 0; j < s.length(); j++) {
+				characterCounter.set(s.charAt(j) - 'a', characterCounter.get(s.charAt(j) - 'a') + 1);
+			}
+			if (indicator.containsKey(characterCounter)) {
+				int index = indicator.get(characterCounter);
+				anagrams.get(index).add(s);
 			} else {
-				int insertionIndex = anagrams.size();
-				anagrams.add(new ArrayList<String>(List.of(s)));
-				indicator.put(chars, insertionIndex);
+				int newIndex = anagrams.size();
+				anagrams.add(new ArrayList<>(List.of(s)));
+				indicator.put(characterCounter, newIndex);
 			}
 		}
 
