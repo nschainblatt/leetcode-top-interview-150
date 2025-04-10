ef555ca60295bd02fda0203cdb2afab1a1c09c9b
nate <nschainblatt@gmail.com>
2025-04-10 22:40:00 +0000

    improve solution

--- two-sum-ii-input-array-is-sorted/main.py
+++ two-sum-ii-input-array-is-sorted/main.py
@@ -6,20 +6,19 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        # If element + maximum possible element is less than target, increase left
-        new_num_index = 0
-        left = 0
-        while left < len(numbers):
-            changed = False
-            for right in range(left+1, len(numbers)):
-                if numbers[new_num_index] == numbers[left] and numbers[new_num_index] != numbers[right]:
-                    new_num_index = right
-                    changed = True
-                if numbers[left] + numbers[right] == target:
-                    return [left+1, right+1]
-            left = (new_num_index if changed else left + 1)
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            v = numbers[left] + numbers[right]
+            if v == target:
+                return [left+1, right+1]
+            if v < target:
+                left += 1
+            else:
+                right -= 1
 
         raise Exception()
+
 
 def main():
     s = Solution()
