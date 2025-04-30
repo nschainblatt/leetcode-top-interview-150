e575a8eae1739dc25ffe71c50ccab43bebe25721
nate <nschainblatt@gmail.com>
2025-04-30 21:08:10 +0000

    improve solution

--- reverse-integer/main.py
+++ reverse-integer/main.py
@@ -1,5 +1,6 @@
 import math
 
+
 class Solution(object):
     def reverse(self, x):
         """
@@ -7,35 +8,17 @@ class Solution(object):
         :rtype: int
         """
 
-        # Turn int to string
-        x_str = str(x)
+        x_str_rev = str(x)[::-1]
+        if x_str_rev[-1] == '-':
+            x_str_rev = '-' + x_str_rev[:-1]
 
-        # Reverse the characters
-        x_str_rev = reverse_string(x_str)
-
-
-        # Turn back into int
         x_int = int(x_str_rev)
 
-        # Check if it's a 32 bit integer
-        # Return 0 if not 32 bit integer
         return x_int if is_32_bit_signed_int(x_int) else 0
 
 
 def is_32_bit_signed_int(x):
     return int(-1 * math.pow(2, 31)) <= x <= int(math.pow(2, 31) - 1)
-
-
-
-def reverse_string(s):
-    r = ""
-    if int(s) < 0:
-        r += "-"
-    for i in range(len(s)-1, -1, -1):
-        if s[i] == '-':
-            continue
-        r += s[i]
-    return r
 
 
 def main():
